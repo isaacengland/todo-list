569054fa2c0f173d538fa51a574a55e460167ef8
isaacengland <iengland368@gmail.com>
2023-01-30 21:22:29 -0600

    Server can now serve from /public

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
+const path = require('path');
 const mongoose = require('mongoose');
+const serveStatic = require('serve-static');
 
 //Database config
 mongoose.set('strictQuery', true);
@@ -11,8 +12,11 @@ const db = mongoose.connection;
 db.on('error', (err) => console.error(err));
 db.once('open', () => console.log('Connected to database'));
 
+//Middleware
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
+//Routes
 const tasksRouter = require('./routes/tasks');
 app.use('/tasks', tasksRouter);
 
